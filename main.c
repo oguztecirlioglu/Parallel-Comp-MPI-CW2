1fe79a294a614591b66810f59f6a219056f73eda
Oguz Tecirlioglu <tecirliogluoguz@gmail.com>
2021-12-26 17:12:26 +0000

    Added local rank computation.

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 int main(int argc, char** argv) {
     int rc, myrank, nproc, namelen;
+    bool firstIteration = true;
     char name[MPI_MAX_PROCESSOR_NAME];
 
     rc = MPI_Init(&argc,  &argv);
@@ -14,17 +15,40 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
     MPI_Get_processor_name(name, &namelen);
     
-    if(myrank == 0) {
-        printf("I am the head node! \n");
-    }
-
-    MPI_Barrier(MPI_COMM_WORLD);
-    printf("I am processor %s, I am rank %i out of %i processors.\n", name, myrank, nproc);
-
     struct inputStruct inputs;
     inputs.numOfProcessors = nproc;
     handleInput(argc, argv, &inputs);
 
+
+
+    /* rank 0 will be the main thread, running all the workers, and checking the precision. */
+    if(myrank == 0) {
+        
+        // This is the first iteration. Node 0 should generate the random square matrix, cut it up accordingly, and distribute it across the procs. 
+        if(firstIteration == true) {
+            firstIteration = false;
+            
+        }
+
+        // Check to see if all other ranks (which are workers) report that they have converged.
+        // If so, collect the pieces of the matrix, and stitch together.
+        else if(firstIteration == false) {
+
+        }
+    } 
+    else {
+        struct rankRowInfo myRankRowInfo = computeResponsibleRows(myrank, inputs.dimension, nproc);
+        
+        if(firstIteration == true) {
+
+        }
+
+        else if(firstIteration == false) {
+            
+        }
+
+        printf("My rank is %i, Start Row: %i, End Row: %i \n", myRankRowInfo.rank, myRankRowInfo.startRow, myRankRowInfo.endRow);
+    }
 
     // Finalize the MPI environment.
     MPI_Finalize();
@@ -74,6 +98,31 @@ void handleInput(int argc, char *argv[], struct inputStruct *input) {
         printf("INPUT ERROR: There are too many cores for this job, will only be using %i cores actively.\n", input->dimension - 2);
     }
 
-    printf("Size is: %i\n", input->dimension);
-    printf("Desired precision is: %f\n", input->precision);
+    //printf("Size is: %i\n", input->dimension);
+    //printf("Desired precision is: %f\n", input->precision);
+}
+
+struct rankRowInfo computeResponsibleRows(int rank, int dimension, int numOfProcs) {
+    int workerProcCOUNT = numOfProcs-1;      // Count of worker processes. DO I NEED TO REDUCE THIS IF NPROC IS BIGGER THAN DIMENSION?
+    int rowsToRelaxCOUNT = dimension - 2;    // Rows to relax, meaning it excludes the boundary rows. 
+    int extraRowCOUNT = rowsToRelaxCOUNT % workerProcCOUNT;     // This many procs need to take care of an additional row.
+    int startRow, endRow, height = rowsToRelaxCOUNT / workerProcCOUNT;
+
+    if(rank <= extraRowCOUNT + 1) {
+        startRow = ((rank-1)*(height+1)) + 1;
+        if(rank <= extraRowCOUNT) {
+            height++;
+        }
+        endRow = startRow + height - 1;
+    }
+    else {
+        startRow = ((extraRowCOUNT) * (height+1)) + ((rank - extraRowCOUNT - 1) * height) + 1;
+        endRow = startRow + height - 1;
+    }
+    struct rankRowInfo result;
+    result.startRow = startRow;
+    result.endRow = endRow;
+    result.rank = rank;
+
+    return result;
 }
--- main.h
+++ main.h
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <mpi.h>
 #include <unistd.h> //Header file optarg.
-#include <stdlib.h> //For atoi 
-
+#include <stdlib.h> //For atoi.
+#include <stdbool.h> //For bool.
 
 /* This is the struct inputs will be loaded into. Rather than using global variables,
     the struct will be initialised in main and its reference will be passed to the functions
@@ -13,5 +13,12 @@ struct inputStruct {
     double precision;
 };
 
+struct rankRowInfo {
+    int startRow;
+    int endRow;
+    int rank;
+};
+
 
 void handleInput(int argc, char *argv[], struct inputStruct *input);
+struct rankRowInfo computeResponsibleRows(int rank, int dimension, int numOfProcs);
